0d62b813d240bb66764e0a0ae8b2366548479a4f
Wiratama Abisatya <abisatya90@gmail.com>
2024-09-29 11:40:12 +0700

    Update: Content

--- src/app.py
+++ src/app.py
@@ -3,20 +3,25 @@ import yaml
 
 app = Flask(__name__)
 
-global content
-with open('src/content.yaml', 'r') as file:
-    resume_content = yaml.safe_load(file)
+
+def load_content():
+    with open('src/content.yaml', 'r') as file:
+        resume_content = yaml.safe_load(file)
+    return resume_content
 
 @app.route('/')
 def index():
+    resume_content = load_content()
     return render_template('index.html', resume=resume_content)
 
 @app.route('/resume')
 def resume():
+    resume_content = load_content()
     return render_template('resume.html', resume=resume_content)
 
 @app.route('/projects')
 def projects():
+    resume_content = load_content()
     return render_template('projects.html', resume=resume_content)
 
 if __name__ == '__main__':
